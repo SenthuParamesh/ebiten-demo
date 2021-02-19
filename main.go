b8ff0c0aecd76cc5904e3e933042fa321434a05f
Senthuran Parameshwaran <senthuren98@gmail.com>
2021-02-19 19:26:11 +0530

    Work in Progress

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"image"
 	_ "image/png"
 	"log"
 
@@ -11,47 +12,241 @@ import (
 )
 
 const (
-	// Settings for the game screen
-	screenWidth  = 960
-	screenHeight = 540
+	// Settings for the ugame screen
+	screenWidth  = 1500
+	screenHeight = 400
+)
+
+const (
+	//	unit    = 160
+	wallLeftX  = 0
+	wallRightX = 1470
+	groundY    = 320
 )
 
 var (
-	leftSprite      *ebiten.Image
-	rightSprite     *ebiten.Image
-	idleSprite      *ebiten.Image
 	backgroundImage *ebiten.Image
+	marioImage      *ebiten.Image
 )
 
-func Init() {
-	// Preload main character images for each direction
-	// Right Image
+func loadAssets() {
+
 	var err error
-	rightSprite, _, err = ebitenutil.NewImageFromFile("Assets/right.png")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Left Image
-	leftSprite, _, err = ebitenutil.NewImageFromFile("Assets/left.png")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Middle Image
-	idleSprite, _, err = ebitenutil.NewImageFromFile("Assets/middle.png")
-	if err != nil {
-		log.Fatal(err)
-	}
 
 	// Background Image
-	backgroundImage, _, err = ebitenutil.NewImageFromFile("Assets/background.png")
+	backgroundImage, _, err = ebitenutil.NewImageFromFile("imgs/world.png")
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalln(err)
+	}
+
+	marioImage, _, err = ebitenutil.NewImageFromFile("imgs/mario.png")
+	if err != nil {
+		log.Fatalln(err)
 	}
 
 }
 
+type Mario struct {
+	Position struct {
+		X float64
+		Y float64
+	}
+	Sprite     *ebiten.Image
+	Animations struct {
+		Idle  *Animation
+		Small struct {
+			WalkRight *Animation
+			WalkLeft  *Animation
+			JumpRight *Animation
+			JumpLeft  *Animation
+		}
+		Large struct {
+			WalkRight *Animation
+			WalkLeft  *Animation
+			JumpRight *Animation
+			JumpLeft  *Animation
+		}
+	}
+	CurrentAnimation *Animation
+}
+
+func createMario(mi *ebiten.Image) *Mario {
+	mario := &Mario{}
+	mario.Position.X = 100
+	mario.Position.Y = 285
+	mario.Sprite = mi
+	mario.Animations.Idle = newIdleAnimation(mario.Sprite)
+	mario.Animations.Large.WalkRight = newLargeWalkRightAnimation(mario.Sprite)
+	mario.CurrentAnimation = mario.Animations.Idle
+	return mario
+}
+
+func (m *Mario) GetAnimationFrameImage() *ebiten.Image {
+	return m.CurrentAnimation.GetFrameImage()
+}
+
+func (m *Mario) ChangePositionX(x float64) {
+	m.Position.X += x
+	if m.Position.X < wallLeftX {
+		m.Position.X = wallLeftX
+	}
+
+	if m.Position.X > wallRightX {
+		m.Position.X = wallRightX
+	}
+}
+
+type Animation struct {
+	CurrentFrame int
+	Period       int
+	Frames       []*Frame
+	n            int
+}
+
+func newIdleAnimation(mi *ebiten.Image) *Animation {
+	anim := &Animation{}
+
+	idleFrame := &Frame{}
+	idleFrame.Image = mi.SubImage(image.Rect(208, 50, 228, 85)).(*ebiten.Image)
+
+	anim.Frames = append(anim.Frames, idleFrame)
+	return anim
+}
+
+func newLargeWalkRightAnimation(mi *ebiten.Image) *Animation {
+	anim := &Animation{}
+	anim.Period = 10
+
+	frame1 := &Frame{}
+	frame1.Image = mi.SubImage(image.Rect(238, 50, 258, 85)).(*ebiten.Image)
+
+	frame2 := &Frame{}
+	frame2.Image = mi.SubImage(image.Rect(268, 50, 288, 85)).(*ebiten.Image)
+
+	frame3 := &Frame{}
+	frame3.Image = mi.SubImage(image.Rect(298, 50, 318, 85)).(*ebiten.Image)
+
+	anim.Frames = []*Frame{frame3, frame2, frame1}
+	return anim
+}
+
+func (a *Animation) Advance() {
+
+	if a.Period == 0 {
+		return
+	}
+
+	a.n++
+	if a.n < a.Period {
+		return
+	}
+
+	a.n = 0
+	a.CurrentFrame = a.CurrentFrame + 1
+
+	if a.CurrentFrame >= len(a.Frames) {
+		a.CurrentFrame = 0
+	}
+}
+
+func (a *Animation) GetFrameImage() *ebiten.Image {
+	return a.Frames[a.CurrentFrame].Image
+}
+
+type Frame struct {
+	Image *ebiten.Image
+}
+
+// Interface to run the game
+func main() {
+
+	loadAssets()
+
+	ebiten.SetWindowSize(screenWidth, screenHeight)
+	ebiten.SetWindowTitle("Mario Game")
+
+	game := &Game{}
+	game.Mario = createMario(marioImage)
+
+	if err := ebiten.RunGame(game); err != nil {
+		panic(err)
+	}
+
+	fmt.Println()
+}
+
+type Game struct {
+	Mario *Mario
+}
+
+// Update() updates the game logic by 1 tick (60 ticks per second)
+func (g *Game) Update() error {
+
+	// Controls
+	if ebiten.IsKeyPressed(ebiten.KeyA) || ebiten.IsKeyPressed(ebiten.KeyLeft) { // "||" OR Operator
+		g.Mario.ChangePositionX(-4)
+	} else if ebiten.IsKeyPressed(ebiten.KeyD) || ebiten.IsKeyPressed(ebiten.KeyRight) {
+		g.Mario.ChangePositionX(4)
+		g.Mario.CurrentAnimation = g.Mario.Animations.Large.WalkRight
+	} else {
+		g.Mario.CurrentAnimation = g.Mario.Animations.Idle
+	}
+
+	if inpututil.IsKeyJustPressed(ebiten.KeySpace) {
+	}
+
+	// change animation
+
+	g.Mario.CurrentAnimation.Advance()
+	return nil
+}
+
+// Draw() draws the screen based on the current game state
+func (g *Game) Draw(screen *ebiten.Image) {
+
+	// Draws Background Image
+	op := &ebiten.DrawImageOptions{}
+	op.GeoM.Scale(2, 2)
+	screen.DrawImage(backgroundImage, op)
+
+	g.Mario.Draw(screen)
+
+}
+
+func (m *Mario) Draw(screen *ebiten.Image) {
+	op := &ebiten.DrawImageOptions{}
+	op.GeoM.Scale(2, 2)                           // Scale matrix by
+	op.GeoM.Translate(m.Position.X, m.Position.Y) // (0,0) is the top-left corner
+	screen.DrawImage(m.GetAnimationFrameImage(), op)
+}
+
+// Layout() gets the window size and return the game logical screen size
+func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
+	return screenWidth, screenHeight
+}
+
+/*
+func (g *Game) Update() error {
+	if g.mario == nil {
+		g.mario = &char{x: 50 * unit, y: groundY * unit}
+	}
+
+	// Controls
+	if ebiten.IsKeyPressed(ebiten.KeyA) || ebiten.IsKeyPressed(ebiten.KeyLeft) { // "||" OR Operator
+		g.mario.vx = -2 * unit
+	} else if ebiten.IsKeyPressed(ebiten.KeyD) || ebiten.IsKeyPressed(ebiten.KeyRight) {
+		g.mario.vx = 2 * unit
+	}
+	if inpututil.IsKeyJustPressed(ebiten.KeySpace) {
+		g.mario.tryJump()
+	}
+	g.mario.update()
+	return nil
+}
+*/
+// Game Loop : It’s an infinite loop that updates and redraws to animate gameplay
+
+/*
 const (
 	unit    = 160
 	groundY = 400
@@ -152,14 +347,4 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
 	return screenWidth, screenHeight
 }
 
-// Interface to run the game
-func main() {
-	Init()
-	ebiten.SetWindowSize(screenWidth, screenHeight)
-	ebiten.SetWindowTitle("Mario Game")
-	if err := ebiten.RunGame(&Game{}); err != nil {
-		panic(err)
-	}
-}
-
-// Game Loop : It’s an infinite loop that updates and redraws to animate gameplay
+*/
